1069f106c347cca5a0c3fede2aec5a9497125d2f
harioctav <aldiamahari16@gmail.com>
2024-11-08 22:35:14 +0700

    Setup Server & Installing Package

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<h1 class="title">Latest Categories</h1>
+	</main>
 </template>
