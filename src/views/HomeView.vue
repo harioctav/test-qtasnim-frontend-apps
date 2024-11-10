0069a9f34d3c429a8d4dd95ee4cfb1cec7034234
harioctav <aldiamahari16@gmail.com>
2024-11-10 10:57:42 +0700

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,10 @@
 	<main>
 		<h1 class="title">This Home Pages</h1>
 
-		<!--  -->
+		<p class="text-center">
+			Ini adalah halaman awal Proyek yang saya buat. Dimana aplikasi ini dibuat
+			untuk memenuhi kebutuhan Test Coding Full Stack Web Developer di PT.
+			QTASNIM DIGITAL TEKNOLOGI atas Nama Pembuat: Aldiama Hari Octavian
+		</p>
 	</main>
 </template>
